e8fdcf32ca04318c7a89c02701d55801cae3427a
divineamunega <divineamunega@gmail.com>
2025-06-06 23:25:05 +0100

    added prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import ProfilePage from "./pages/ProfilePage";
+
 const App = function () {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
